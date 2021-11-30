5f2345f7c8f6ee201a74014c5b3de1bf08a7dcc8
Cyl <labetowiez@aol.fr>
2021-11-30 15:07:03 +0100

    added the top of the temple

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $statement = $pdo->prepare($query);
     $statement->bindValue("name", $name["name"], \PDO::PARAM_STR);
     $statement->execute();
+    header("Location:");
 }
 
 $query = "select name from argonaute";
@@ -18,7 +19,7 @@ $names = $statement->fetchAll();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
@@ -30,27 +31,36 @@ $names = $statement->fetchAll();
 
 <body>
     <!-- Header section -->
-    <header>
+    <header class="header">
         <h1>
-            <img src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png" alt="Wild Code School logo" />
             Les Argonautes
         </h1>
+        <svg viewBox="0 0 960 200">
+            <polygon class="secondary-clr" points="50 160, 480 20, 910 160" />
+            <polygon class="primary-clr" points="110 150, 480 30, 850 150" />
+            <polygon class="primary-clr" points="50 160, 480 20, 910 160, 915 140, 480 0, 45, 140" />
+            <polygon class="primary-clr" points="70 160, 890 160, 890 180, 70 180" />
+            <line class="ceiling" x1="50" y1="160" x2="480" y2="20" />
+            <line class="ceiling" x1="480" y1="20" x2="910" y2="160" />
+            <line class="inner left" x1="110" y1="150" x2="480" y2="30" />
+            <line class="inner right" x1="480" y1="30" x2="850" y2="150" />
+            <line class="inner bottom" x1="110" y1="150" x2="850" y2="150" />
+        </svg>
     </header>
 
     <!-- Main section -->
-    <main>
-
+    <main class="inside">
         <!-- New member form -->
-        <h2>Ajouter un(e) Argonaute</h2>
         <form class="new-member-form" method="POST">
+            <h2>Ajouter un(e) Argonaute</h2>
             <label for="name">Nom de l&apos;Argonaute</label>
             <input id="name" name="name" type="text" placeholder="Charalampos" />
             <button type="submit">Envoyer</button>
         </form>
 
         <!-- Member list -->
-        <h2>Membres de l'équipage</h2>
         <section class="member-list">
+            <h2>Membres de l'équipage</h2>
             <?php foreach ($names as $name) : ?>
                 <div class="member-item"><?= $name["name"] ?></div>
             <?php endforeach; ?>
